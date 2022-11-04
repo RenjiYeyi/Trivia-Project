cb51831520683072c01e98461965f450bb20892c
Regina Pelaez <regis.pelaez.calzado@gmail.com>
2022-11-03 19:56:02 -0600

    Primer subida a repo
    Se creo la estructura base del HTML, CSS y las primeras funciones en JS

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function colorChange1(event) {
+  event.preventDefault();
+  let card1 = document.querySelector("#tarjeta1");
+  card1.classList.add("active");
+  document.getElementById("popup1").style.display = "block";
+  document.body.classList.add("stop-scrolling");
+}
+
+function answerChange(event) {
+  event.preventDefault();
+  console.log("hola mundo")
+  let answer1 = document.querySelector("#respuesta1");
+  answer1.classList.add("resalte");
+}
+
+function closePop(event){
+  event.preventDefault();
+  document.getElementById("popup1").style.display = "none";
+}
+
+
+let result1 = document.querySelector("#tarjeta1");
+result1.addEventListener("click", colorChange1);
+
+let resalte = document.querySelector(".resultado");
+resalte.addEventListener("click", answerChange);
+
+let done = document.querySelector(".aceptar");
+done.addEventListener("click", closePop)
+
+
